065e3c7320d897672ef9457688b1bf5814ec0ce5
Isaac Watson <iawatson@oakland.edu>
2018-09-22 16:25:45 -0400

    User Input
    Gabe can have basic movement now

---
+++ GrizzHacks2018/Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    protected Vector2 direction;
+
+    [SerializeField]
+    private float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        moveChar();
+	}
+
+    private void moveChar()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+}
---
+++ GrizzHacks2018/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        getInput();
+        base.Update();
+	}
+
+    private void getInput()
+    {
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+
+
+    }
+}
