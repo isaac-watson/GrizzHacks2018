160d8ef24f25e31020b25500d71b775cc58185db
Jacoba777 <jacobraustin98@gmail.com>
2018-09-23 11:05:33 -0400

    Revert "Merge branch 'master' of https://github.com/isaac-watson/GrizzHacks2018"
    This reverts commit 024ff5c7b667f20a0775b70e66a6c01bd08888a0, reversing
    changes made to 65fc6d67675fed056ee59b4c8da441f54de732ea.

--- GrizzHacks2018/Assets/Scripts/AttackProjectile.cs
+++ GrizzHacks2018/Assets/Scripts/AttackProjectile.cs
@@ -5,36 +5,24 @@ using UnityEngine;
 public class AttackProjectile : MonoBehaviour {
 
     
-    private float decay = 3f;
-    Vector3 oldPos;
+    private float decay = 6f;
     Vector3 newPos;
-    Animator anim;
 	// Use this for initialization
 	void Start () {
         
-        //gameObject.SetActive(false);
-
+        gameObject.SetActive(false);
         
 	}
-
-    void OnEnable()
-    {
-        transform.position.Set(0.88f, 0.89f, 0);
-        anim = GetComponent<Animator>();
-        anim.Play("projectile");
-    }
 	
 	// Update is called once per frame
 	void Update () {
-        
-            MoveRight();
+        MoveRight();
     }
 
     private void MoveRight()
     {
         if (transform.position == newPos)
             RemoveProjectile();
-        oldPos = transform.position;
         newPos = new Vector3(3f, 0.89f, 0);
         transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * decay);
         
@@ -43,10 +31,7 @@ public class AttackProjectile : MonoBehaviour {
 
     private void RemoveProjectile()
     {
-        
-    
         gameObject.SetActive(false);
-
     }
 
 }
