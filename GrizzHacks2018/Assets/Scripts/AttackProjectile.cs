42df92b18c025b20b36af717ab9f49bf34f1ad24
Isaac Watson <iawatson@oakland.edu>
2018-09-23 08:46:04 -0400

    Added projectile to attack

---
+++ GrizzHacks2018/Assets/Scripts/AttackProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackProjectile : MonoBehaviour {
+
+    
+    private float decay = 6f;
+    Vector3 newPos;
+	// Use this for initialization
+	void Start () {
+        
+        gameObject.SetActive(false);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveRight();
+    }
+
+    private void MoveRight()
+    {
+        if (transform.position == newPos)
+            RemoveProjectile();
+        newPos = new Vector3(3f, 0.89f, 0);
+        transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * decay);
+        
+        
+    }
+
+    private void RemoveProjectile()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
